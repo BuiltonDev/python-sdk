28dcd0af3ac1805f385cd7eb921567e7d810e483
Kristopher <kris_jor@hotmail.com>
2018-12-06 14:59:35 +0100

    check webhooks

--- examples/example.py
+++ examples/example.py
@@ -2,6 +2,7 @@ from random import randint
 from examples.config import ENDPOINT, API_KEY, BEARER_TOKEN
 from src.main import Kvass
 
+
 k = Kvass(endpoint=ENDPOINT, api_key=API_KEY, bearer_token=BEARER_TOKEN)
 users = k.user().get_all()
 print(users)
