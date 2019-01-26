7f34fc8c4920f7414dea9eea3a06a7ed641dd63a
Ronald Kaiser <raios.catodicos@gmail.com>
2019-01-26 18:52:12 +0100

    refactoring test_components.py

--- tests/unit/test_components.py
+++ tests/unit/test_components.py
@@ -3,6 +3,9 @@ import pytest
 from src.collection.components import Components
 
 
+class Component(Components): pass
+
+
 # __init__
 def test_cannot_construct_abstract_instances():
     with pytest.raises(Exception):
@@ -10,61 +13,42 @@ def test_cannot_construct_abstract_instances():
 
 
 def test_init_sets_parameters():
-    class Comp(Components):
-        pass
-
-    comp = Comp("request", None)
-    assert comp.identifier == None
-    assert comp.request == "request"
-    assert comp.id == None
-    assert comp.api_path == None
+    component = Component("request", None)
+    assert component.identifier == None
+    assert component.request == "request"
+    assert component.id == None
+    assert component.api_path == None
 
 
 def test_string_props_turns_into_id():
-    class Comp(Components):
-        pass
-
-    comp = Comp(None, "props")
-    assert comp.id == "props"
+    component = Component(None, "props")
+    assert component.id == "props"
 
 
 def test_dict_props_calls_build_instance(mocker):
-    class Comp(Components):
-        pass
     mocker.patch.object(Components, 'build_instance')
     props = {'foo' : 'bar'}
-    Comp(None, props)
+    Component(None, props)
     Components.build_instance.assert_called_once_with(props)
 
 
 # build_instance
 def test_dict_props_turns_into_instance_props():
-    class Comp(Components):
-        pass
     props = {'foo' : 'bar', 'monty' : 42}
-    comp = Comp(None, props)
-    assert comp.foo == 'bar'
-    assert comp.monty == 42
+    component = Component(None, props)
+    assert component.foo == 'bar'
+    assert component.monty == 42
 
 
 def test_id_oid_from_props_turns_into_instance_prop():
-    class Comp(Components):
-        pass
-
     props = {'_id' : {'$oid' : 42}}
-    comp = Comp(None, props)
-    assert comp.id == 42
+    component = Component(None, props)
+    assert component.id == 42
 
 
 # simple_query
 def test_simple_query_without_id_raises_exception():
-    class Comp(Components):
-        pass
-
-    comp = Comp(None, None)
+    component = Component(None, None)
     with pytest.raises(ValueError):
-        comp.simple_query(_id=None)
-
-
-
+        component.simple_query(_id=None)
 
