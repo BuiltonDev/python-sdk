5233cdf1e72b5d81a384a0014a100c59dbd1709e
Ronald Kaiser <raios.catodicos@gmail.com>
2019-06-15 11:25:09 +0200

    version 0.2.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(name='builton-sdk',
-      version='0.2.0',
+      version='0.2.1',
       description='BuiltOn Python SDK',
       long_description=long_description,
       long_description_content_type="text/markdown",
