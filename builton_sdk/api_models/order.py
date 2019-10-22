42460db6d45350f1ddc6ba7bc487da8368737dbd
alchemistake <9083268+alchemistake@users.noreply.github.com>
2019-10-22 13:29:17 +0200

    "DELETE" on orders are not used.

--- builton_sdk/api_models/order.py
+++ builton_sdk/api_models/order.py
@@ -2,8 +2,8 @@ from builton_sdk.api_models.component import Component
 from builton_sdk.utils.rest_functions import *
 
 
-@rest_decorator(get, create, delete, get_all, refresh, update, search)
-class Order(Component):
+@rest_decorator(get, create, get_all, refresh, update, search)
+class Order(_Component):
     def __init__(self, request, props):
         super(Order, self).__init__(request, props)
         self.api_path = 'orders'
